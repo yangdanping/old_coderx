b45c910c72efd851a6c94883290f1b53cee69f10
yangdanping <1240645840@qq.com>
2021-11-16 17:27:14 +0800

    1.0

--- src/global/config.js
+++ src/global/config.js
@@ -4,8 +4,8 @@ console.log('当前环境', process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   BASE_URL = '/api'; //开发阶段已配置proxy选项解决跨域问题,生产环境到时再说
 } else if (process.env.NODE_ENV === 'production') {
-  // BASE_URL = '/api';
-  BASE_URL = 'http://119.91.150.141:8000';
+  BASE_URL = '/api';
+  // BASE_URL = 'http://119.91.150.141:8000';
 }
 
 export { BASE_URL, TIME_OUT };
