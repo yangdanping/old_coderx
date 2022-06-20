6410c43ccc193112c1730be899311992d5999b29
yangdanping <1240645840@qq.com>
2022-06-20 18:42:56 +0800

    添加跨域配置

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,11 @@ module.exports = {
     // 配置proxy-------解决开发阶段跨域访问的问题(注意,只要是改了配置都要重新编译)
     proxy: {
       //这里配置好后要在service的config文件中的BASE_URL改为'/api'
+      '/api': {
+        target: 'http://localhost:8000', //接口的前缀
+        pathRewrite: { '^/dev-api': '' }, //重写路径
+        changeOrigin: true //虚拟的站点需要更管origin
+      },
       '/dev-api': {
         target: 'http://localhost:8000', //接口的前缀
         pathRewrite: { '^/dev-api': '' }, //重写路径
