e0951ffcc67d461d07395f8ed9dd073de620fec9
yangdanping <42269873+yangdanping@users.noreply.github.com>
2023-05-13 02:33:26 +0800

    api端口改为9000

--- vue.config.js
+++ vue.config.js
@@ -6,12 +6,12 @@ module.exports = {
     proxy: {
       //这里配置好后要在service的config文件中的BASE_URL改为'/api'
       '/api': {
-        target: 'http://119.91.150.141:8000', //接口的前缀
+        target: 'http://119.91.150.141:9000', //接口的前缀
         pathRewrite: { '^/api': '' }, //重写路径
         changeOrigin: true //虚拟的站点需要更管origin
       },
       '/dev-api': {
-        target: 'http://localhost:8000', //接口的前缀
+        target: 'http://localhost:9000', //接口的前缀
         pathRewrite: { '^/dev-api': '' }, //重写路径
         changeOrigin: true //虚拟的站点需要更管origin
       },
